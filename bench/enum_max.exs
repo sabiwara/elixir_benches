0ee6b1f8c8d5e8145eace763ade97d34372723f7
sabiwara <sabiwara@gmail.com>
2021-02-11 18:45:32 +0900

    Bench using otp24 (with JIT)

--- bench/enum_max.exs
+++ bench/enum_max.exs
@@ -3,7 +3,5 @@ list = Enum.to_list(range)
 
 Benchee.run(%{
   ":lists.max/1 (list)" => fn -> :lists.max(list) end,
-  "Enum.max/1 (list)" => fn -> Enum.max(list) end,
-  "FastEnum.max/1 (list)" => fn -> FastEnum.max(list) end,
-  "FastEnum.max/2 (list with fallback)" => fn -> FastEnum.max(list, fn -> 0 end) end,
+  "Enum.max/1 (list)" => fn -> Enum.max(list) end
 })
