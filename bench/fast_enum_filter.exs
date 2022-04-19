b801710b8529ba51aa636b812152b6d2294fd6af
sabiwara <sabiwara@gmail.com>
2022-04-19 22:30:03 +0900

    Update filter bench

--- bench/fast_enum_filter.exs
+++ bench/fast_enum_filter.exs
@@ -1,5 +1,5 @@
-list = Enum.to_list(1..100)
-fun = &(rem(&1, 4) == 0)
+list = Enum.to_list(1..1000)
+fun = &(rem(&1, 2) == 0)
 
 Benchee.run(%{
   "Enum.filter/2" => fn -> Enum.filter(list, fun) end,
