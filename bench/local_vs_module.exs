7bbdbdb4d448264ecc8f88cabb11f1932d691bb2
sabiwara <sabiwara@gmail.com>
2024-06-15 15:37:11 +0900

    Update bench for local/remote

--- bench/local_vs_module.exs
+++ bench/local_vs_module.exs
@@ -1,18 +1,17 @@
-defmodule Foo do
-  def foo(x), do: 1 + x
-end
-
 defmodule Bench do
   # uncomment to get S file
   # @compile :S
 
   def module(enum) do
-    Enum.each(enum, &Foo.foo/1)
+    Enum.each(enum, &__MODULE__.foo/1)
   end
 
   def local(enum) do
     Enum.each(enum, &foo/1)
   end
+
+  def local_closure, do: &foo/1
+  def remote_closure, do: &__MODULE__.foo/1
 
   def foo(x), do: 1 + x
 end
@@ -21,10 +20,15 @@ inputs = [
   {"list", Enum.to_list(1..1000)}
 ]
 
+local_closure = Bench.local_closure()
+remote_closure = Bench.remote_closure()
+
 Benchee.run(
   [
     module: &Bench.module/1,
-    local: &Bench.local/1
+    local: &Bench.local/1,
+    local_closure: &Enum.each(&1, local_closure),
+    remote_closure: &Enum.each(&1, remote_closure)
   ],
   inputs: inputs
 )
