ccf6af580926a017f5b3cfb3686b9b62a08f9f47
sabiwara <sabiwara@gmail.com>
2024-12-31 09:23:29 +0900

    Bench JSON using chars rather than strings

---
+++ bench/json.exs
@@ -0,0 +1,12 @@
+ten_obj = Enum.map(1..10, &%{"foo#{&1}" => "bar#{&1}"})
+
+Benchee.run(
+  [
+    # "json iodata": &:erl_json.encode/1,
+    # "faster iodata": &:faster_json.encode/1,
+    "json binary": &IO.iodata_to_binary(:erl_json.encode(&1)),
+    "faster binary": &IO.iodata_to_binary(:faster_json.encode(&1))
+  ],
+  inputs: [{"10 objects", ten_obj}],
+  memory_time: 0.5
+)
