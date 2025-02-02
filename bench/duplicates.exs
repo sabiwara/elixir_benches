cb324c4d52bcd354adf415d02c905a9e236c1b59
sabiwara <sabiwara@gmail.com>
2025-02-02 17:52:53 +0900

    Benchmark new approach to duplicates

--- bench/duplicates.exs
+++ bench/duplicates.exs
@@ -7,6 +7,21 @@ defmodule Bench do
     for {x, c} when c > 1 <- Enum.frequencies(enum), do: x
   end
 
+  def sort_dupes(enum) when is_list(enum) do
+    :lists.sort(enum) |> sorted_dupes()
+  end
+
+  defp sorted_dupes([]), do: []
+
+  defp sorted_dupes([x, x | xs]) do
+    [x | skip(x, xs) |> sorted_dupes()]
+  end
+
+  defp sorted_dupes([_ | xs]), do: sorted_dupes(xs)
+
+  defp skip(x, [x | xs]), do: skip(x, xs)
+  defp skip(_x, other), do: other
+
   def duplicates_body(enum) when is_list(enum) do
     list_duplicates(enum, %{})
   end
@@ -43,12 +58,20 @@ inputs = [
   {"big no duplicate", Enum.to_list(1..100_000)}
 ]
 
+# l = Enum.concat(1..50, 50..1//-1)
+
+# d = Bench.duplicates_body(l) |> dbg()
+# ^d = Bench.duplicates_tail(l)
+# s = Enum.sort(d)
+# ^s = Bench.sort_dupes(l) |> dbg()
+
 Benchee.run(
   %{
     "naive_uniq" => &Bench.naive_uniq/1,
     "naive_frequencies" => &Bench.naive_freq/1,
     "duplicates (body recursive)" => &Bench.duplicates_body/1,
-    "duplicates (tail recursive)" => &Bench.duplicates_tail/1
+    "duplicates (tail recursive)" => &Bench.duplicates_tail/1,
+    "sort_dupes" => &Bench.sort_dupes/1
   },
   inputs: inputs,
   memory_time: 0.5
