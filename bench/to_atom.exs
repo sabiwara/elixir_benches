c16abb7836ca4d6122aa6e139ed8c7b33f274853
sabiwara <sabiwara@proton.me>
2026-06-12 08:40:06 +0900

    Include to_existing_atom + membership check

--- bench/to_atom.exs
+++ bench/to_atom.exs
@@ -5,6 +5,16 @@ defmodule Bench do
     Enum.find(@atoms, &(Atom.to_string(&1) == string)) || raise "invalid"
   end
 
+  def member(string) do
+    atom = String.to_existing_atom(string)
+
+    if not Enum.member?(@atoms, atom) do
+      raise "invalid"
+    end
+
+    atom
+  end
+
   for atom <- @atoms do
     def match(unquote(Atom.to_string(atom))), do: unquote(atom)
   end
@@ -17,6 +27,7 @@ strings = Bench.atoms() |> Enum.map(&to_string/1) |> Enum.shuffle()
 
 Benchee.run(%{
   "Enum.find" => fn -> Enum.each(strings, &Bench.find/1) end,
+  "Enum.member?" => fn -> Enum.each(strings, &Bench.member/1) end,
   "pattern-matching" => fn -> Enum.each(strings, &Bench.match/1) end,
   "String.to_existing_atom" => fn -> Enum.each(strings, &String.to_existing_atom/1) end
 })
