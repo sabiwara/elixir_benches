e69733a2c8125d8530ce3cae2742e1d4b1559b9b
sabiwara <sabiwara@proton.me>
2026-06-12 20:32:47 +0900

    Benchmark Map.get approach too

--- bench/to_atom.exs
+++ bench/to_atom.exs
@@ -19,6 +19,13 @@ defmodule Bench do
     def match(unquote(Atom.to_string(atom))), do: unquote(atom)
   end
 
+  def match(_), do: raise("invalid")
+
+  @lookup_map Map.new(@atoms, &{Atom.to_string(&1), &1})
+  def map_get(string) do
+    Map.get(@lookup_map, string) || raise "invalid"
+  end
+
   def atoms, do: @atoms
 end
 
@@ -29,5 +36,6 @@ Benchee.run(%{
   "Enum.find" => fn -> Enum.each(strings, &Bench.find/1) end,
   "Enum.member?" => fn -> Enum.each(strings, &Bench.member/1) end,
   "pattern-matching" => fn -> Enum.each(strings, &Bench.match/1) end,
+  "Map.get" => fn -> Enum.each(strings, &Bench.map_get/1) end,
   "String.to_existing_atom" => fn -> Enum.each(strings, &String.to_existing_atom/1) end
 })
